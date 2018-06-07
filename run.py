09140e0666aa03f38766ad9dd2a26b37071570b1
Ethan Houston <ethan.houston@gmail.com>
2018-06-06 23:33:30 -0700

    Initial commit

---
+++ run.py
@@ -0,0 +1,37 @@
+
+# import the necessary packages
+from __future__ import print_function
+from skimage.feature import peak_local_max
+from skimage.morphology import watershed
+from scipy import ndimage
+import argparse
+import cv2
+
+
+# load the image and perform pyramid mean shift filtering
+# to aid the thresholding step
+image = cv2.imread("C:\\Users\\Ethan\\Code\\clover-classifier\\images\\patch.jpg")
+shifted = cv2.pyrMeanShiftFiltering(image, 21, 51)
+
+# convert the mean shift image to grayscale, then apply
+# Otsu's thresholding
+gray = cv2.cvtColor(shifted, cv2.COLOR_BGR2GRAY)
+thresh = cv2.threshold(gray, 0, 255,
+	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+# find contours in the thresholded image
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)[-2]
+print("[INFO] {} unique contours found".format(len(cnts)))
+
+# loop over the contours
+for (i, c) in enumerate(cnts):
+    # draw the contour
+    ((x, y), _) = cv2.minEnclosingCircle(c)
+    cv2.putText(image, "#{}".format(i + 1), (int(x) - 10, int(y)),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+
+# show the output image
+cv2.imshow("Image", image)
+cv2.waitKey(0)
